f0967a96605c596a13d2bb5613fd54144d4c800b
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2022-05-07 22:15:36 +0300

    section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Main } from "../../components/Layouts";
+import { useLanguage } from "../../hooks";
 
 const Home = () => {
+  const language = useLanguage();
   return (
     <Main>
       <section className="pt-5">
@@ -44,6 +46,42 @@ const Home = () => {
                 I design and code beautifully simple things, and I love what I
                 do.
               </p>
+            </Col>
+            <Col lg={4} sm={6} xs={8} className="text-center mb-4 mb-lg-4">
+              <Row>
+                <Col xs={12}>
+                  <h4> language {language.language} </h4>
+                </Col>
+              </Row>
+              <Row className="justify-content-center px-4">
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+              </Row>
+            </Col>
+            <Col lg={4} sm={6} xs={8} className="text-center mb-4 mb-lg-4">
+              <Row>
+                <Col xs={12}>
+                  <h4> language {language.language} </h4>
+                </Col>
+              </Row>
+              <Row className="justify-content-center px-4">
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+              </Row>
+            </Col>
+            <Col lg={4} sm={6} xs={8} className="text-center mb-4 mb-lg-4">
+              <Row>
+                <Col xs={12}>
+                  <h4> language {language.language} </h4>
+                </Col>
+              </Row>
+              <Row className="justify-content-center px-4">
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+                <Col xs={4}><img className="w-100" src="./images/HTML-logo.svg" /></Col>
+              </Row>
             </Col>
           </Row>
         </Container>
