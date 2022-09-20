39857ff101da84067263703a62c5bbed80e3ff0a
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2022-09-20 16:54:29 +0300

    done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import { images } from "assets";
 import { Main } from "components/Layouts";
 import { useLanguage, useTheme } from "hooks";
 import about from "data/about.json";
 import projects from "data/projects.json";
-import { Link } from "react-router-dom";
+import { Eye, Link45deg, ThreeDots } from "react-bootstrap-icons";
 
 const Home = () => {
   const language = useLanguage();
   const theme = useTheme();
+  const [modalImage, setModalImage] = React.useState("");
+  const [modalProject, setModalProject] = React.useState(null);
 
   return (
     <Main id="home_page">
@@ -102,26 +104,102 @@ const Home = () => {
                     className="text-center mb-4 mb-lg-4"
                     key={index}
                   >
-                    <Row>
-                      <Col xs={12} className="mb-4">
-                        <h4 style={{ color: theme.text }}>{project?.name}</h4>
-                      </Col>
-                      <Col xs={12} key={index}>
-                        <Link to={project.url}>
-                          <img
-                            className="w-100"
-                            alt="html"
-                            src={project?.image}
-                          />
-                        </Link>
-                      </Col>
-                    </Row>
+                    <div className="project-cart">
+                      <Row>
+                        <Col xs={12} key={index} className="mb-4">
+                          <div className="project-img-container position-relative shadow rounded-1">
+                            <img
+                              className="w-100 project-img rounded-1"
+                              alt="html"
+                              src={project?.image}
+                              height={250}
+                            />
+                            <div className="project-overlay position-absolute w-100  bg-overlay top-0 rounded-1 h-100">
+                              <ul className="list-unstyled gap-3 d-flex align-items-center justify-content-center h-100 mb-0">
+                                <li className="bg-white rounded-circle p-1">
+                                  <Button
+                                    className="p-0"
+                                    onClick={() =>
+                                      setModalImage(project?.image)
+                                    }
+                                    variant="link"
+                                  >
+                                    <Eye
+                                      color="darkgray"
+                                      size={20}
+                                      className="project-icon"
+                                    />
+                                  </Button>
+                                </li>
+                                <li className="bg-white rounded-circle p-1">
+                                  <a
+                                    href={project.url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                  >
+                                    <Link45deg
+                                      color="darkgray"
+                                      size={20}
+                                      className="project-icon"
+                                    />
+                                  </a>
+                                </li>
+                                <li className="bg-white rounded-circle p-1">
+                                  <Button
+                                    className="p-0"
+                                    onClick={() => setModalProject(project)}
+                                    variant="link"
+                                  >
+                                    <ThreeDots
+                                      color="darkgray"
+                                      size={20}
+                                      className="project-icon"
+                                    />
+                                  </Button>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12}>
+                          <h4 style={{ color: theme.text }}>
+                            <a
+                              href={project.url}
+                              target="_blank"
+                              rel="noreferrer"
+                              className="text-black"
+                            >
+                              {project?.name[language.code]}
+                            </a>
+                          </h4>
+                        </Col>
+                      </Row>
+                    </div>
                   </Col>
                 ))}
               </Row>
             </Col>
           </Row>
         </Container>
+
+        <Modal
+          show={modalImage !== ""}
+          onHide={() => setModalImage("")}
+          size="lg"
+        >
+          <div>
+            <img className="w-100" alt="html" src={modalImage} />
+          </div>
+        </Modal>
+        <Modal
+          show={modalProject}
+          onHide={() => setModalProject(null)}
+          size="lg"
+        >
+          <div>
+            {/* <img className="w-100" alt="html" src={modalProject} /> */}
+          </div>
+        </Modal>
       </section>
     </Main>
   );
